060de96dce8c1a9dc01090aa2253e01f116aab40
CharlesPikachu <1159254961@qq.com>
2022-07-13 06:32:56 +0800

    only support distributed train and test

--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $THIS_DIR
-cd ..
-CFGFILEPATH=$1
-CHECKPOINTSPATH=$2
-NOEVAL=$3
-python3 ssseg/test.py --cfgfilepath $CFGFILEPATH --checkpointspath $CHECKPOINTSPATH ${3}
--- scripts/train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-THIS_DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd $THIS_DIR
-cd ..
-CFGFILEPATH=$1
-CHECKPOINTSPATH=${2:-"latest.pth"}
-python3 ssseg/train.py --cfgfilepath $CFGFILEPATH --checkpointspath $CHECKPOINTSPATH
