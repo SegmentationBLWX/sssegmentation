b4692a9ae01560ec86b6b4aa87c66061ae15dfa1
CharlesPikachu <1159254961@qq.com>
2022-08-04 23:54:42 +0800

    support l1 loss

--- ssseg/modules/models/segmentors/base/base.py
+++ ssseg/modules/models/segmentors/base/base.py
@@ -168,7 +168,7 @@ class BaseSegmentor(nn.Module):
             if (key in ['binaryceloss']) and hasattr(self, 'onehot'):
                 prediction_iter = prediction_format
                 target_iter = self.onehot(target, self.cfg['num_classes'])
-            elif key in ['diceloss', 'lovaszloss', 'kldivloss']:
+            elif key in ['diceloss', 'lovaszloss', 'kldivloss', 'l1loss']:
                 prediction_iter = prediction
                 target_iter = target
             else:
