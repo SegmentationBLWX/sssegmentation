7b8e89c8d70e74aeddd5114c2acc48db177c2686
CharlesPikachu <1159254961@qq.com>
2023-05-31 09:20:25 +0800

    fix mobilenet config

--- ssseg/configs/pspnet/pspnet_mobilenetv2os8_voc.py
+++ ssseg/configs/pspnet/pspnet_mobilenetv2os8_voc.py
@@ -1,19 +1,19 @@
 '''pspnet_mobilenetv2os8_voc'''
 import copy
 from .base_cfg import SEGMENTOR_CFG
-from .._base_ import DATASET_CFG_ADE20k_512x512, DATALOADER_CFG_BS16
+from .._base_ import DATASET_CFG_VOCAUG_512x512, DATALOADER_CFG_BS16
 
 
 # deepcopy
 SEGMENTOR_CFG = copy.deepcopy(SEGMENTOR_CFG)
 # modify dataset config
-SEGMENTOR_CFG['dataset'] = DATASET_CFG_ADE20k_512x512.copy()
+SEGMENTOR_CFG['dataset'] = DATASET_CFG_VOCAUG_512x512.copy()
 # modify dataloader config
 SEGMENTOR_CFG['dataloader'] = DATALOADER_CFG_BS16.copy()
 # modify scheduler config
-SEGMENTOR_CFG['scheduler']['max_epochs'] = 130
+SEGMENTOR_CFG['scheduler']['max_epochs'] = 60
 # modify other segmentor configs
-SEGMENTOR_CFG['num_classes'] = 150
+SEGMENTOR_CFG['num_classes'] = 21
 SEGMENTOR_CFG['backbone'] = {
     'type': 'MobileNetV2', 'structure_type': 'mobilenetv2', 'pretrained': True, 'outstride': 8, 'selected_indices': (2, 3),
 }
