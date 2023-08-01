3020f8295ec18fec29634e391a940ef1940166ea
CharlesPikachu <1159254961@qq.com>
2023-08-01 21:36:50 +0800

    update logger module

--- ssseg/modules/utils/logger.py
+++ ssseg/modules/utils/logger.py
@@ -14,22 +14,24 @@ class Logger():
         self.logfilepath = logfilepath
         self.fp_handler = open(logfilepath, 'a')
     '''log'''
-    def log(self, message, level='INFO'):
-        message = f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())} {level} {message}'
+    def log(self, message, level='INFO', endwithnewline=True):
+        message = f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())} {level}  {message}'
+        if not message.endswith('\n') and endwithnewline:
+            message = message + '\n'
         print(message)
         self.fp_handler.write(message)
     '''debug'''
-    def debug(self, message):
-        self.log(message, 'DEBUG')
+    def debug(self, message, endwithnewline=True):
+        self.log(message, 'DEBUG', endwithnewline)
     '''info'''
-    def info(self, message):
-        self.log(message, 'INFO')
+    def info(self, message, endwithnewline=True):
+        self.log(message, 'INFO', endwithnewline)
     '''warning'''
-    def warning(self, message):
-        self.log(message, 'WARNING')
+    def warning(self, message, endwithnewline=True):
+        self.log(message, 'WARNING', endwithnewline)
     '''error'''
-    def error(self, message):
-        self.log(message, 'ERROR')
+    def error(self, message, endwithnewline=True):
+        self.log(message, 'ERROR', endwithnewline)
     '''close'''
     def close(self):
         self.fp_handler.close()
