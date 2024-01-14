0f5d5d0aed2ff4d92de9bfca31f35d2e956f0f9f
CharlesPikachu <1159254961@qq.com>
2024-01-14 11:45:56 +0800

    update dnlnet configs

--- ssseg/configs/dnlnet/dnlnet_resnet101os16_ade20k.py
+++ ssseg/configs/dnlnet/dnlnet_resnet101os16_ade20k.py
@@ -1,4 +1,5 @@
 '''dnlnet_resnet101os16_ade20k'''
+import os
 import copy
 from .base_cfg import SEGMENTOR_CFG
 from .._base_ import DATASET_CFG_ADE20k_512x512, DATALOADER_CFG_BS16
@@ -18,6 +19,6 @@ SEGMENTOR_CFG['backbone'] = {
     'type': 'ResNet', 'depth': 101, 'structure_type': 'resnet101conv3x3stem',
     'pretrained': True, 'outstride': 16, 'use_conv3x3_stem': True, 'selected_indices': (2, 3),
 }
-SEGMENTOR_CFG['work_dir'] = 'dnlnet_resnet101os16_ade20k'
-SEGMENTOR_CFG['logfilepath'] = 'dnlnet_resnet101os16_ade20k/dnlnet_resnet101os16_ade20k.log'
-SEGMENTOR_CFG['resultsavepath'] = 'dnlnet_resnet101os16_ade20k/dnlnet_resnet101os16_ade20k_results.pkl'
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
--- ssseg/configs/dnlnet/dnlnet_resnet101os8_cityscapes.py
+++ ssseg/configs/dnlnet/dnlnet_resnet101os8_cityscapes.py
@@ -1,4 +1,5 @@
 '''dnlnet_resnet101os8_cityscapes'''
+import os
 import copy
 from .base_cfg import SEGMENTOR_CFG
 from .._base_ import DATASET_CFG_CITYSCAPES_512x1024, DATALOADER_CFG_BS8
@@ -14,6 +15,6 @@ SEGMENTOR_CFG['dataloader'] = DATALOADER_CFG_BS8.copy()
 SEGMENTOR_CFG['scheduler']['max_epochs'] = 220
 # modify other segmentor configs
 SEGMENTOR_CFG['num_classes'] = 19
-SEGMENTOR_CFG['work_dir'] = 'dnlnet_resnet101os8_cityscapes'
-SEGMENTOR_CFG['logfilepath'] = 'dnlnet_resnet101os8_cityscapes/dnlnet_resnet101os8_cityscapes.log'
-SEGMENTOR_CFG['resultsavepath'] = 'dnlnet_resnet101os8_cityscapes/dnlnet_resnet101os8_cityscapes_results.pkl'
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
