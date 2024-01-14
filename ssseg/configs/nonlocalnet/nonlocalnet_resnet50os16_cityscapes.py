c09abda2fa69a04c0a3a38add46b6e349a4aee9b
CharlesPikachu <1159254961@qq.com>
2024-01-14 13:15:31 +0800

    update nonlocal configs

--- ssseg/configs/nonlocalnet/nonlocalnet_resnet50os16_cityscapes.py
+++ ssseg/configs/nonlocalnet/nonlocalnet_resnet50os16_cityscapes.py
@@ -1,4 +1,5 @@
 '''nonlocalnet_resnet50os16_cityscapes'''
+import os
 import copy
 from .base_cfg import SEGMENTOR_CFG
 from .._base_ import DATASET_CFG_CITYSCAPES_512x1024, DATALOADER_CFG_BS8
@@ -18,6 +19,6 @@ SEGMENTOR_CFG['backbone'] = {
     'type': 'ResNet', 'depth': 50, 'structure_type': 'resnet50conv3x3stem',
     'pretrained': True, 'outstride': 16, 'use_conv3x3_stem': True, 'selected_indices': (2, 3),
 }
-SEGMENTOR_CFG['work_dir'] = 'nonlocalnet_resnet50os16_cityscapes'
-SEGMENTOR_CFG['logfilepath'] = 'nonlocalnet_resnet50os16_cityscapes/nonlocalnet_resnet50os16_cityscapes.log'
-SEGMENTOR_CFG['resultsavepath'] = 'nonlocalnet_resnet50os16_cityscapes/nonlocalnet_resnet50os16_cityscapes_results.pkl'
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
--- ssseg/configs/nonlocalnet/nonlocalnet_resnet50os16_voc.py
+++ ssseg/configs/nonlocalnet/nonlocalnet_resnet50os16_voc.py
@@ -1,4 +1,5 @@
 '''nonlocalnet_resnet50os16_voc'''
+import os
 import copy
 from .base_cfg import SEGMENTOR_CFG
 from .._base_ import DATASET_CFG_VOCAUG_512x512, DATALOADER_CFG_BS16
@@ -18,6 +19,6 @@ SEGMENTOR_CFG['backbone'] = {
     'type': 'ResNet', 'depth': 50, 'structure_type': 'resnet50conv3x3stem',
     'pretrained': True, 'outstride': 16, 'use_conv3x3_stem': True, 'selected_indices': (2, 3),
 }
-SEGMENTOR_CFG['work_dir'] = 'nonlocalnet_resnet50os16_voc'
-SEGMENTOR_CFG['logfilepath'] = 'nonlocalnet_resnet50os16_voc/nonlocalnet_resnet50os16_voc.log'
-SEGMENTOR_CFG['resultsavepath'] = 'nonlocalnet_resnet50os16_voc/nonlocalnet_resnet50os16_voc_results.pkl'
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
