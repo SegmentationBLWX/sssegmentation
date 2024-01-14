eb6bc35765ca51effb3eb9eb0c65c6a0272b714c
CharlesPikachu <1159254961@qq.com>
2024-01-14 13:17:53 +0800

    update pointrend  configs

--- ssseg/configs/pointrend/pointrend_resnet101_ade20k.py
+++ ssseg/configs/pointrend/pointrend_resnet101_ade20k.py
@@ -1,4 +1,5 @@
 '''pointrend_resnet101_ade20k'''
+import os
 import copy
 from .base_cfg import SEGMENTOR_CFG
 from .._base_ import DATASET_CFG_ADE20k_512x512, DATALOADER_CFG_BS16
@@ -14,6 +15,6 @@ SEGMENTOR_CFG['dataloader'] = DATALOADER_CFG_BS16.copy()
 SEGMENTOR_CFG['scheduler']['max_epochs'] = 130
 # modify other segmentor configs
 SEGMENTOR_CFG['num_classes'] = 150
-SEGMENTOR_CFG['work_dir'] = 'pointrend_resnet101_ade20k'
-SEGMENTOR_CFG['logfilepath'] = 'pointrend_resnet101_ade20k/pointrend_resnet101_ade20k.log'
-SEGMENTOR_CFG['resultsavepath'] = 'pointrend_resnet101_ade20k/pointrend_resnet101_ade20k_results.pkl'
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
