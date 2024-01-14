186c8339f0b6da40ead2ad5884bcad73c0e18841
CharlesPikachu <1159254961@qq.com>
2024-01-14 11:33:57 +0800

    update apc configs

--- ssseg/configs/apcnet/apcnet_resnet101os16_voc.py
+++ ssseg/configs/apcnet/apcnet_resnet101os16_voc.py
@@ -1,4 +1,5 @@
 '''apcnet_resnet101os16_voc'''
+import os
 import copy
 from .base_cfg import SEGMENTOR_CFG
 from .._base_ import DATASET_CFG_VOCAUG_512x512, DATALOADER_CFG_BS16
@@ -18,6 +19,6 @@ SEGMENTOR_CFG['backbone'] = {
     'type': 'ResNet', 'depth': 101, 'structure_type': 'resnet101conv3x3stem',
     'pretrained': True, 'outstride': 16, 'use_conv3x3_stem': True, 'selected_indices': (2, 3),
 }
-SEGMENTOR_CFG['work_dir'] = 'apcnet_resnet101os16_voc'
-SEGMENTOR_CFG['logfilepath'] = 'apcnet_resnet101os16_voc/apcnet_resnet101os16_voc.log'
-SEGMENTOR_CFG['resultsavepath'] = 'apcnet_resnet101os16_voc/apcnet_resnet101os16_voc_results.pkl'
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
