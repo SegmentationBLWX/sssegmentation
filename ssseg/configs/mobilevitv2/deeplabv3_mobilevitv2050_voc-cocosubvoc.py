b932a9cfab08de1e9a73568cb65b75ee51392c16
CharlesPikachu <1159254961@qq.com>
2024-01-18 13:08:14 +0800

    add mobilevitv2 configs

--- ssseg/configs/mobilevitv2/deeplabv3_mobilevitv2050_voc-cocosubvoc.py
+++ ssseg/configs/mobilevitv2/deeplabv3_mobilevitv2050_voc-cocosubvoc.py
@@ -2,24 +2,24 @@
 import os
 import copy
 from .base_cfg import SEGMENTOR_CFG
-from .._base_ import DATASET_CFG_VOCAUG_512x512, DATASET_CFG_COCOVOCSUB_512x512, DATALOADER_CFG_BS64
+from .._base_ import DATASET_CFG_VOCAUG_MOBILEVIT_512x512, DATASET_CFG_COCOVOCSUB_MOBILEVIT_512x512, DATALOADER_CFG_BS64
 
 
 # deepcopy
 SEGMENTOR_CFG = copy.deepcopy(SEGMENTOR_CFG)
 # modify dataset config
-DATASET_CFG_VOCAUG_512x512 = DATASET_CFG_VOCAUG_512x512.copy()
-DATASET_CFG_COCOVOCSUB_512x512 = DATASET_CFG_COCOVOCSUB_512x512.copy()
-DATASET_CFG_VOCAUG_512x512['seg_target_remapper'] = {i: i for i in range(21)}
-DATASET_CFG_COCOVOCSUB_512x512['seg_target_remapper'] = {i: i for i in range(21)}
+DATASET_CFG_VOCAUG_MOBILEVIT_512x512 = DATASET_CFG_VOCAUG_MOBILEVIT_512x512.copy()
+DATASET_CFG_COCOVOCSUB_MOBILEVIT_512x512 = DATASET_CFG_VOCAUG_MOBILEVIT_512x512.copy()
+DATASET_CFG_VOCAUG_MOBILEVIT_512x512['seg_target_remapper'] = {i: i for i in range(21)}
+DATASET_CFG_COCOVOCSUB_MOBILEVIT_512x512['seg_target_remapper'] = {i: i for i in range(21)}
 SEGMENTOR_CFG['dataset'] = {
     'type': 'MultipleDataset',
     'train': {
-        'VOCDataset': DATASET_CFG_VOCAUG_512x512.copy(),
-        'COCOVOCSUBDataset': DATASET_CFG_COCOVOCSUB_512x512.copy(),
+        'VOCDataset': DATASET_CFG_VOCAUG_MOBILEVIT_512x512.copy(),
+        'COCOVOCSUBDataset': DATASET_CFG_COCOVOCSUB_MOBILEVIT_512x512.copy(),
     },
     'test': {
-        'VOCDataset': DATASET_CFG_VOCAUG_512x512.copy(),
+        'VOCDataset': DATASET_CFG_VOCAUG_MOBILEVIT_512x512.copy(),
     }
 }
 # modify dataloader config
