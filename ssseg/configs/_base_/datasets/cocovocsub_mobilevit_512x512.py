3c68859019cb5ee3dfcf2fab8a530e3cb8df47c4
CharlesPikachu <1159254961@qq.com>
2024-01-18 22:58:01 +0800

    update

--- ssseg/configs/_base_/datasets/cocovocsub_mobilevit_512x512.py
+++ ssseg/configs/_base_/datasets/cocovocsub_mobilevit_512x512.py
@@ -9,7 +9,7 @@ DATASET_CFG_COCOVOCSUB_MOBILEVIT_512x512 = {
     'train': {
         'set': 'train',
         'data_pipelines': [
-            ('Resize', {'output_size': (2048, 512), 'keep_ratio': True, 'scale_range': (0.5, 2.0)}),
+            ('RandomChoiceResize', {'scales': [int(512 * x * 0.1) for x in range(5, 21)], 'resize_type': 'ResizeShortestEdge', 'max_size': 2048}),
             ('RandomCrop', {'crop_size': (512, 512), 'one_category_max_ratio': 0.75}),
             ('RandomFlip', {'flip_prob': 0.5}),
             ('RandomRotation', {'angle_upper': 10, 'rotation_prob': 0.5}),
