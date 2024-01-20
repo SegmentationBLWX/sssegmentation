7268bf3822d3da0bdb87a53e64534b2cc009ca55
CharlesPikachu <1159254961@qq.com>
2024-01-20 14:52:52 +0800

    make fix for building from source

--- ssseg/__init__.py
+++ ssseg/__init__.py
@@ -17,15 +17,18 @@ __copyright__ = 'Copyright 2020-2030 Zhenchao Jin'
 
 
 '''import all'''
-from . import configs
-from .test import Tester
-from .train import Trainer
-from .inference import Inferencer
-from .modules import (
-    DatasetBuilder, DataTransformBuilder, BuildDataset, BuildDataTransform, BuildDistributedDataloader, BuildDistributedModel,
-    LossBuilder, BackboneBuilder, SegmentorBuilder, PixelSamplerBuilder, OptimizerBuilder, SchedulerBuilder, ParamsConstructorBuilder, 
-    NormalizationBuilder, ActivationBuilder, DropoutBuilder, BuildLoss, BuildBackbone, BuildSegmentor, BuildPixelSampler, BuildOptimizer, 
-    BuildScheduler, BuildParamsConstructor, BuildNormalization, BuildActivation, BuildDropout, EMASegmentor, EnvironmentCollector,
-    initslurm, setrandomseed, touchdir, loadckpts, saveckpts, loadpretrainedweights, symlink, judgefileexist, postprocesspredgtpairs,
-    LoggerHandleBuilder, BuildLoggerHandle, TrainingLoggingManager, BaseModuleBuilder, ConfigParser
-)
+try:
+    from . import configs
+    from .test import Tester
+    from .train import Trainer
+    from .inference import Inferencer
+    from .modules import (
+        DatasetBuilder, DataTransformBuilder, BuildDataset, BuildDataTransform, BuildDistributedDataloader, BuildDistributedModel,
+        LossBuilder, BackboneBuilder, SegmentorBuilder, PixelSamplerBuilder, OptimizerBuilder, SchedulerBuilder, ParamsConstructorBuilder, 
+        NormalizationBuilder, ActivationBuilder, DropoutBuilder, BuildLoss, BuildBackbone, BuildSegmentor, BuildPixelSampler, BuildOptimizer, 
+        BuildScheduler, BuildParamsConstructor, BuildNormalization, BuildActivation, BuildDropout, EMASegmentor, EnvironmentCollector,
+        initslurm, setrandomseed, touchdir, loadckpts, saveckpts, loadpretrainedweights, symlink, judgefileexist, postprocesspredgtpairs,
+        LoggerHandleBuilder, BuildLoggerHandle, TrainingLoggingManager, BaseModuleBuilder, ConfigParser
+    )
+except:
+    pass
