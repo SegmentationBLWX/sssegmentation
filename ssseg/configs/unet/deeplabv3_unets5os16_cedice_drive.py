53222d6ce2b07fca206cb73be085827694bf2bd3
CharlesPikachu <1159254961@qq.com>
2024-10-23 03:02:02 +0800

    more configs with dice loss for unet

---
+++ ssseg/configs/unet/deeplabv3_unets5os16_cedice_drive.py
@@ -0,0 +1,39 @@
+'''deeplabv3_unets5os16_cedice_drive'''
+import os
+import copy
+from .base_cfg import SEGMENTOR_CFG
+from .._base_ import DATASET_CFG_DRIVE_64x64, DATALOADER_CFG_BS16
+
+
+# deepcopy
+SEGMENTOR_CFG = copy.deepcopy(SEGMENTOR_CFG)
+# modify dataset config
+SEGMENTOR_CFG['dataset'] = DATASET_CFG_DRIVE_64x64.copy()
+# modify dataloader config
+SEGMENTOR_CFG['dataloader'] = DATALOADER_CFG_BS16.copy()
+# modify scheduler config
+SEGMENTOR_CFG['scheduler']['max_epochs'] = 1
+# modify other segmentor configs
+SEGMENTOR_CFG['type'] = 'Deeplabv3'
+SEGMENTOR_CFG['num_classes'] = 2
+SEGMENTOR_CFG['head'] = {
+    'in_channels': 64, 'feats_channels': 16, 'dilations': [1, 6, 12, 18], 'dropout': 0.1,
+}
+SEGMENTOR_CFG['losses'] = {
+    'loss_aux': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean'},
+    'loss_cls': [
+        {'type': 'CrossEntropyLoss', 'scale_factor': 1.0, 'ignore_index': 255, 'reduction': 'mean'},
+        {'type': 'DiceLoss', 'scale_factor': 3.0, 'ignore_index': 255, 'reduction': 'mean'},
+    ],
+}
+SEGMENTOR_CFG['inference'] = {
+    'mode': 'slide',
+    'opts': {'cropsize': (64, 64), 'stride': (42, 42)}, 
+    'tricks': {
+        'multiscale': [1], 'flip': False, 'use_probs_before_resize': True
+    },
+    'evaluate': {'metric_list': ['dice', 'mdice']},
+}
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
---
+++ ssseg/configs/unet/pspnet_unets5os16_cedice_stare.py
@@ -0,0 +1,39 @@
+'''pspnet_unets5os16_cedice_stare'''
+import os
+import copy
+from .base_cfg import SEGMENTOR_CFG
+from .._base_ import DATASET_CFG_STARE_128x128, DATALOADER_CFG_BS16
+
+
+# deepcopy
+SEGMENTOR_CFG = copy.deepcopy(SEGMENTOR_CFG)
+# modify dataset config
+SEGMENTOR_CFG['dataset'] = DATASET_CFG_STARE_128x128.copy()
+# modify dataloader config
+SEGMENTOR_CFG['dataloader'] = DATALOADER_CFG_BS16.copy()
+# modify scheduler config
+SEGMENTOR_CFG['scheduler']['max_epochs'] = 1
+# modify other segmentor configs
+SEGMENTOR_CFG['type'] = 'PSPNet'
+SEGMENTOR_CFG['num_classes'] = 2
+SEGMENTOR_CFG['head'] = {
+    'in_channels': 64, 'feats_channels': 16, 'pool_scales': [1, 2, 3, 6], 'dropout': 0.1,
+}
+SEGMENTOR_CFG['losses'] = {
+    'loss_aux': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean'},
+    'loss_cls': [
+        {'type': 'CrossEntropyLoss', 'scale_factor': 1.0, 'ignore_index': 255, 'reduction': 'mean'},
+        {'type': 'DiceLoss', 'scale_factor': 3.0, 'ignore_index': 255, 'reduction': 'mean'},
+    ],
+}
+SEGMENTOR_CFG['inference'] = {
+    'mode': 'slide',
+    'opts': {'cropsize': (128, 128), 'stride': (85, 85)}, 
+    'tricks': {
+        'multiscale': [1], 'flip': False, 'use_probs_before_resize': True
+    },
+    'evaluate': {'metric_list': ['dice', 'mdice']},
+}
+SEGMENTOR_CFG['work_dir'] = os.path.split(__file__)[-1].split('.')[0]
+SEGMENTOR_CFG['evaluate_results_filename'] = f"{os.path.split(__file__)[-1].split('.')[0]}.pkl"
+SEGMENTOR_CFG['logger_handle_cfg']['logfilepath'] = os.path.join(SEGMENTOR_CFG['work_dir'], f"{os.path.split(__file__)[-1].split('.')[0]}.log")
