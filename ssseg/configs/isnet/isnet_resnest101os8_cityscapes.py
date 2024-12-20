a454b2e56b19a9313d870297efb41a387b985e76
Zhenchao Jin <blwx96@connect.hku.hk>
2024-12-21 07:58:34 +0800

    update configs

--- ssseg/configs/isnet/isnet_resnest101os8_cityscapes.py
+++ ssseg/configs/isnet/isnet_resnest101os8_cityscapes.py
@@ -28,7 +28,7 @@ SEGMENTOR_CFG['inference'] = SEGMENTOR_CFG['inference'].copy()
 # --multi-scale with flipping
 '''
 SEGMENTOR_CFG['inference'] = {
-    'forward': {'mode': 'slide', 'cropsize': (1024, 512), 'stride': (341, 341)},
+    'forward': {'mode': 'slide', 'cropsize': (512, 1024), 'stride': (341, 341)},
     'tta': {'multiscale': [0.5, 0.75, 1.0, 1.25, 1.5, 1.75], 'flip': True, 'use_probs_before_resize': True},
     'evaluate': {'metric_list': ['iou', 'miou']},
 }
