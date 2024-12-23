2387f9fa6c612f3b14e854ba084b47ef3399604e
Zhenchao Jin <blwx96@connect.hku.hk>
2024-12-24 01:50:33 +0800

    make fix for data aug in ce2p

--- ssseg/configs/ce2p/ce2p_resnet50os16_voc.py
+++ ssseg/configs/ce2p/ce2p_resnet50os16_voc.py
@@ -12,7 +12,7 @@ SEGMENTOR_CFG['dataset'] = DATASET_CFG_VOCAUG_512x512.copy()
 SEGMENTOR_CFG['dataset']['train']['data_pipelines'] = [
     ('Resize', {'output_size': (2048, 512), 'keep_ratio': True, 'scale_range': (0.5, 2.0)}),
     ('RandomCrop', {'crop_size': (512, 512), 'one_category_max_ratio': 0.75}),
-    ('RandomFlip', {'flip_prob': 0.5}),
+    ('RandomFlip', {'prob': 0.5}),
     ('PhotoMetricDistortion', {}),
     ('EdgeExtractor', {}),
     ('Normalize', {'mean': [123.675, 116.28, 103.53], 'std': [58.395, 57.12, 57.375]}),
