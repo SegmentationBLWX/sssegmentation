d762f7f0ced733bf9edba2ed7ac658882bc2d25b
CharlesPikachu <1159254961@qq.com>
2025-06-04 02:00:35 +0800

    advance dataloader config

--- ssseg/configs/_base_/dataloaders/default_dataloader.py
+++ ssseg/configs/_base_/dataloaders/default_dataloader.py
@@ -1,7 +1,7 @@
 '''default_dataloader'''
 from typing import Any, Dict
+from dataclasses import dataclass, field
 from collections.abc import MutableMapping
-from dataclasses import dataclass, field, asdict
 
 
 '''DataloaderConfig'''
@@ -11,21 +11,40 @@ class DataloaderConfig(MutableMapping):
     auto_adapt_to_expected_train_bs: bool
     train: Dict[str, Any] = field(default_factory=dict)
     test: Dict[str, Any] = field(default_factory=dict)
+    _extra: Dict[str, Any] = field(default_factory=dict, repr=False)
+    def __init__(self, expected_total_train_bs_for_assert: int, auto_adapt_to_expected_train_bs: bool, train=None, test=None, **kwargs):
+        self.expected_total_train_bs_for_assert = expected_total_train_bs_for_assert
+        self.auto_adapt_to_expected_train_bs = auto_adapt_to_expected_train_bs
+        self.train = train if train is not None else {}
+        self.test = test if test is not None else {}
+        self._extra = kwargs
     '''getitem'''
     def __getitem__(self, key):
-        return asdict(self)[key]
+        if key in self.__dict__:
+            return getattr(self, key)
+        return self._extra[key]
     '''setitem'''
     def __setitem__(self, key, value):
-        setattr(self, key, value)
+        if key in {'expected_total_train_bs_for_assert', 'auto_adapt_to_expected_train_bs', 'train', 'test'}:
+            setattr(self, key, value)
+        else:
+            self._extra[key] = value
     '''delitem'''
     def __delitem__(self, key):
-        delattr(self, key)
+        if key in self.__dict__:
+            delattr(self, key)
+        elif key in self._extra:
+            del self._extra[key]
+        else:
+            raise KeyError(key)
     '''iter'''
     def __iter__(self):
-        return iter(asdict(self))
+        return iter({**self.__dict__, **self._extra})
     '''len'''
     def __len__(self):
-        return len(asdict(self))
+        return len(self.__dict__) + len(self._extra)
     '''repr'''
     def __repr__(self):
-        return f"DataloaderConfig({asdict(self)})"
+        core = {k: v for k, v in self.__dict__.items() if k != '_extra'}
+        merged = {**core, **self._extra}
+        return f"DataloaderConfig({merged})"
