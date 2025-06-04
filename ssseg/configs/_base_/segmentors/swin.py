2d7d2a0d54e9d83b0d496ac95544f0c823be4570
CharlesPikachu <1159254961@qq.com>
2025-06-04 16:13:02 +0800

    advance segmentor cfgs

--- ssseg/configs/_base_/segmentors/swin.py
+++ ssseg/configs/_base_/segmentors/swin.py
@@ -3,41 +3,41 @@ from .default_segmentor import SegmentorConfig
 
 
 '''SWIN_SEGMENTOR_CFG'''
-SWIN_SEGMENTOR_CFG = {
-    'type': 'UPerNet',
-    'num_classes': -1,
-    'benchmark': True,
-    'align_corners': False,
-    'work_dir': 'ckpts',
-    'eval_interval_epochs': 10,
-    'save_interval_epochs': 1,
-    'logger_handle_cfg': {'type': 'LocalLoggerHandle', 'logfilepath': ''},
-    'training_logging_manager_cfg': {'log_interval_iters': 50},
-    'norm_cfg': {'type': 'SyncBatchNorm'},
-    'act_cfg': {'type': 'ReLU', 'inplace': True},
-    'backbone': {
+SWIN_SEGMENTOR_CFG = SegmentorConfig(
+    type='UPerNet',
+    num_classes=-1,
+    benchmark=True,
+    align_corners=False,
+    work_dir='ckpts',
+    eval_interval_epochs=10,
+    save_interval_epochs=1,
+    logger_handle_cfg={'type': 'LocalLoggerHandle', 'logfilepath': ''},
+    training_logging_manager_cfg={'log_interval_iters': 50},
+    norm_cfg={'type': 'SyncBatchNorm'},
+    act_cfg={'type': 'ReLU', 'inplace': True},
+    backbone={
         'type': 'SwinTransformer', 'structure_type': 'swin_base_patch4_window12_384_22k', 'pretrained': True,
         'selected_indices': (0, 1, 2, 3), 'norm_cfg': {'type': 'LayerNorm'},
         'pretrain_img_size': 384, 'in_channels': 3, 'embed_dims': 128, 'patch_size': 4, 'window_size': 12, 'mlp_ratio': 4,
         'depths': [2, 2, 18, 2], 'num_heads': [4, 8, 16, 32], 'qkv_bias': True, 'qk_scale': None, 'patch_norm': True,
         'drop_rate': 0., 'attn_drop_rate': 0., 'drop_path_rate': 0.3, 'use_abs_pos_embed': False,
     },
-    'head': {
+    head={
         'in_channels_list': [128, 256, 512, 1024], 'feats_channels': 512, 'pool_scales': [1, 2, 3, 6], 'dropout': 0.1,
     },
-    'auxiliary': {
+    auxiliary={
         'in_channels': 512, 'out_channels': 512, 'dropout': 0.1,
     },
-    'losses': {
+    losses={
         'loss_aux': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean'},
         'loss_cls': {'type': 'CrossEntropyLoss', 'scale_factor': 1.0, 'ignore_index': 255, 'reduction': 'mean'},
     },
-    'inference': {
+    inference={
         'forward': {'mode': 'whole', 'cropsize': None, 'stride': None},
         'tta': {'multiscale': [1], 'flip': False, 'use_probs_before_resize': False},
         'evaluate': {'metric_list': ['iou', 'miou']},
     },
-    'scheduler': {
+    scheduler={
         'type': 'PolyScheduler', 'max_epochs': 0, 'power': 1.0, 'min_lr': 0.0,
         'warmup_cfg': {'type': 'linear', 'ratio': 1e-6, 'iters': 1500},
         'optimizer': {
@@ -48,6 +48,6 @@ SWIN_SEGMENTOR_CFG = {
             },
         }
     },
-    'dataset': None,
-    'dataloader': None,
-}
+    dataset=None,
+    dataloader=None,
+)
--- ssseg/configs/_base_/segmentors/twins.py
+++ ssseg/configs/_base_/segmentors/twins.py
@@ -3,38 +3,38 @@ from .default_segmentor import SegmentorConfig
 
 
 '''TWINS_SEGMENTOR_CFG'''
-TWINS_SEGMENTOR_CFG = {
-    'type': 'UPerNet',
-    'num_classes': -1,
-    'benchmark': True,
-    'align_corners': False,
-    'work_dir': 'ckpts',
-    'eval_interval_epochs': 10,
-    'save_interval_epochs': 1,
-    'logger_handle_cfg': {'type': 'LocalLoggerHandle', 'logfilepath': ''},
-    'training_logging_manager_cfg': {'log_interval_iters': 50},
-    'norm_cfg': {'type': 'SyncBatchNorm'},
-    'act_cfg': {'type': 'ReLU', 'inplace': True},
-    'backbone': {
-        'type': 'PCPVT', 'structure_type': 'pcpvt_base', 'pretrained': True, 'selected_indices': (0, 1, 2, 3), 
+TWINS_SEGMENTOR_CFG = SegmentorConfig(
+    type='UPerNet',
+    num_classes=-1,
+    benchmark=True,
+    align_corners=False,
+    work_dir='ckpts',
+    eval_interval_epochs=10,
+    save_interval_epochs=1,
+    logger_handle_cfg={'type': 'LocalLoggerHandle', 'logfilepath': ''},
+    training_logging_manager_cfg={'log_interval_iters': 50},
+    norm_cfg={'type': 'SyncBatchNorm'},
+    act_cfg={'type': 'ReLU', 'inplace': True},
+    backbone={
+        'type': 'PCPVT', 'structure_type': 'pcpvt_base', 'pretrained': True, 'selected_indices': (0, 1, 2, 3),
         'norm_cfg': {'type': 'LayerNorm'}, 'depths': [3, 4, 18, 3], 'drop_path_rate': 0.3,
     },
-    'head': {
+    head={
         'in_channels_list': [64, 128, 320, 512], 'feats_channels': 512, 'pool_scales': [1, 2, 3, 6], 'dropout': 0.1,
     },
-    'auxiliary': {
+    auxiliary={
         'in_channels': 320, 'out_channels': 512, 'dropout': 0.1,
     },
-    'losses': {
+    losses={
         'loss_aux': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean'},
         'loss_cls': {'type': 'CrossEntropyLoss', 'scale_factor': 1.0, 'ignore_index': 255, 'reduction': 'mean'},
     },
-    'inference': {
+    inference={
         'forward': {'mode': 'whole', 'cropsize': None, 'stride': None},
         'tta': {'multiscale': [1], 'flip': False, 'use_probs_before_resize': False},
         'evaluate': {'metric_list': ['iou', 'miou']},
     },
-    'scheduler': {
+    scheduler={
         'type': 'PolyScheduler', 'max_epochs': 0, 'power': 1.0, 'min_lr': 0.0,
         'warmup_cfg': {'type': 'linear', 'ratio': 1e-6, 'iters': 1500},
         'optimizer': {
@@ -45,6 +45,6 @@ TWINS_SEGMENTOR_CFG = {
             },
         }
     },
-    'dataset': None,
-    'dataloader': None,
-}
+    dataset=None,
+    dataloader=None,
+)
