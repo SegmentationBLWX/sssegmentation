7b0300a5b662d32e782ee1b67318e37b287fad33
CharlesPikachu <1159254961@qq.com>
2025-06-04 14:40:30 +0800

    advance segmentor cfgs

--- ssseg/configs/_base_/segmentors/fastscnn.py
+++ ssseg/configs/_base_/segmentors/fastscnn.py
@@ -3,44 +3,44 @@ from .default_segmentor import SegmentorConfig
 
 
 '''FASTSCNN_SEGMENTOR_CFG'''
-FASTSCNN_SEGMENTOR_CFG = {
-    'type': 'DepthwiseSeparableFCN',
-    'num_classes': -1,
-    'benchmark': True,
-    'align_corners': False,
-    'work_dir': 'ckpts',
-    'eval_interval_epochs': 10,
-    'save_interval_epochs': 1,
-    'logger_handle_cfg': {'type': 'LocalLoggerHandle', 'logfilepath': ''},
-    'training_logging_manager_cfg': {'log_interval_iters': 50},
-    'norm_cfg': {'type': 'SyncBatchNorm'},
-    'act_cfg': {'type': 'ReLU', 'inplace': True},
-    'backbone': {
+FASTSCNN_SEGMENTOR_CFG = SegmentorConfig(
+    type='DepthwiseSeparableFCN',
+    num_classes=-1,
+    benchmark=True,
+    align_corners=False,
+    work_dir='ckpts',
+    eval_interval_epochs=10,
+    save_interval_epochs=1,
+    logger_handle_cfg={'type': 'LocalLoggerHandle', 'logfilepath': ''},
+    training_logging_manager_cfg={'log_interval_iters': 50},
+    norm_cfg={'type': 'SyncBatchNorm'},
+    act_cfg={'type': 'ReLU', 'inplace': True},
+    backbone={
         'type': 'FastSCNN', 'structure_type': 'fastscnn', 'pretrained': False, 'selected_indices': (0, 1, 2),
     },
-    'head': {
+    head={
         'in_channels': 128, 'feats_channels': 128, 'dropout': 0.1,
     },
-    'auxiliary': [
-        {'in_channels': 64, 'out_channels': 32, 'dropout': 0.1,},
-        {'in_channels': 128, 'out_channels': 32, 'dropout': 0.1,},
+    auxiliary=[
+        {'in_channels': 64, 'out_channels': 32, 'dropout': 0.1},
+        {'in_channels': 128, 'out_channels': 32, 'dropout': 0.1},
     ],
-    'losses': {
+    losses={
         'loss_aux1': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean', 'use_sigmoid': True},
         'loss_aux2': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean', 'use_sigmoid': True},
         'loss_cls': {'type': 'CrossEntropyLoss', 'scale_factor': 1.0, 'ignore_index': 255, 'reduction': 'mean', 'use_sigmoid': True},
     },
-    'inference': {
+    inference={
         'forward': {'mode': 'whole', 'cropsize': None, 'stride': None},
         'tta': {'multiscale': [1], 'flip': False, 'use_probs_before_resize': False},
         'evaluate': {'metric_list': ['iou', 'miou']},
     },
-    'scheduler': {
+    scheduler={
         'type': 'PolyScheduler', 'max_epochs': 0, 'power': 0.9, 'min_lr': 1e-4,
         'optimizer': {
             'type': 'SGD', 'lr': 0.12, 'momentum': 0.9, 'weight_decay': 4e-5, 'params_rules': {},
         }
     },
-    'dataset': None,
-    'dataloader': None,
-}
+    dataset=None,
+    dataloader=None,
+)
--- ssseg/configs/_base_/segmentors/gcnet.py
+++ ssseg/configs/_base_/segmentors/gcnet.py
@@ -3,44 +3,44 @@ from .default_segmentor import SegmentorConfig
 
 
 '''GCNET_SEGMENTOR_CFG'''
-GCNET_SEGMENTOR_CFG = {
-    'type': 'GCNet',
-    'num_classes': -1,
-    'benchmark': True,
-    'align_corners': False,
-    'work_dir': 'ckpts',
-    'eval_interval_epochs': 10,
-    'save_interval_epochs': 1,
-    'logger_handle_cfg': {'type': 'LocalLoggerHandle', 'logfilepath': ''},
-    'training_logging_manager_cfg': {'log_interval_iters': 50},
-    'norm_cfg': {'type': 'SyncBatchNorm'},
-    'act_cfg': {'type': 'ReLU', 'inplace': True},
-    'backbone': {
+GCNET_SEGMENTOR_CFG = SegmentorConfig(
+    type='GCNet',
+    num_classes=-1,
+    benchmark=True,
+    align_corners=False,
+    work_dir='ckpts',
+    eval_interval_epochs=10,
+    save_interval_epochs=1,
+    logger_handle_cfg={'type': 'LocalLoggerHandle', 'logfilepath': ''},
+    training_logging_manager_cfg={'log_interval_iters': 50},
+    norm_cfg={'type': 'SyncBatchNorm'},
+    act_cfg={'type': 'ReLU', 'inplace': True},
+    backbone={
         'type': 'ResNet', 'depth': 101, 'structure_type': 'resnet101conv3x3stem',
         'pretrained': True, 'outstride': 8, 'use_conv3x3_stem': True, 'selected_indices': (2, 3),
     },
-    'head': {
+    head={
         'in_channels': 2048, 'feats_channels': 512, 'ratio': 0.25, 'pooling_type': 'att',
         'fusion_types': ('channel_add', ), 'norm_cfg': {'type': 'LayerNorm'}, 'dropout': 0.1,
     },
-    'auxiliary': {
+    auxiliary={
         'in_channels': 1024, 'out_channels': 512, 'dropout': 0.1,
     },
-    'losses': {
+    losses={
         'loss_aux': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean'},
         'loss_cls': {'type': 'CrossEntropyLoss', 'scale_factor': 1.0, 'ignore_index': 255, 'reduction': 'mean'},
     },
-    'inference': {
+    inference={
         'forward': {'mode': 'whole', 'cropsize': None, 'stride': None},
         'tta': {'multiscale': [1], 'flip': False, 'use_probs_before_resize': False},
         'evaluate': {'metric_list': ['iou', 'miou']},
     },
-    'scheduler': {
+    scheduler={
         'type': 'PolyScheduler', 'max_epochs': 0, 'power': 0.9,
         'optimizer': {
             'type': 'SGD', 'lr': 0.01, 'momentum': 0.9, 'weight_decay': 5e-4, 'params_rules': {},
         }
     },
-    'dataset': None,
-    'dataloader': None,
-}
+    dataset=None,
+    dataloader=None,
+)
