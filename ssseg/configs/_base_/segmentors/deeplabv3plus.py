32a23dcf79a41ef06b7eb0c16e2cca0e7a277688
CharlesPikachu <1159254961@qq.com>
2025-06-04 09:12:08 +0800

    advance segmentor configs

---
+++ ssseg/configs/_base_/segmentors/deeplabv3plus.py
@@ -0,0 +1,45 @@
+'''SEGMENTOR_CFG for Deeplabv3Plus'''
+from .default_segmentor import SegmentorConfig
+
+
+'''DEEPLABV3PLUS_SEGMENTOR_CFG'''
+DEEPLABV3PLUS_SEGMENTOR_CFG = SegmentorConfig(
+    benchmark=True,
+    num_classes=-1,
+    align_corners=False,
+    type='Deeplabv3Plus',
+    work_dir='ckpts',
+    eval_interval_epochs=10,
+    save_interval_epochs=1,
+    logger_handle_cfg={'type': 'LocalLoggerHandle', 'logfilepath': ''},
+    training_logging_manager_cfg={'log_interval_iters': 50},
+    norm_cfg={'type': 'SyncBatchNorm'},
+    act_cfg={'type': 'ReLU', 'inplace': True},
+    backbone={
+        'type': 'ResNet', 'depth': 101, 'structure_type': 'resnet101conv3x3stem',
+        'pretrained': True, 'outstride': 8, 'use_conv3x3_stem': True, 'selected_indices': (0, 1, 2, 3),
+    },
+    head={
+        'in_channels': [256, 2048], 'feats_channels': 512, 'shortcut_channels': 48, 'dilations': [1, 6, 12, 18], 'dropout': 0.1,
+    },
+    auxiliary={
+        'in_channels': 1024, 'out_channels': 512, 'dropout': 0.1,
+    },
+    losses={
+        'loss_aux': {'type': 'CrossEntropyLoss', 'scale_factor': 0.4, 'ignore_index': 255, 'reduction': 'mean'},
+        'loss_cls': {'type': 'CrossEntropyLoss', 'scale_factor': 1.0, 'ignore_index': 255, 'reduction': 'mean'},
+    },
+    inference={
+        'forward': {'mode': 'whole', 'cropsize': None, 'stride': None},
+        'tta': {'multiscale': [1], 'flip': False, 'use_probs_before_resize': False},
+        'evaluate': {'metric_list': ['iou', 'miou']},
+    },
+    scheduler={
+        'type': 'PolyScheduler', 'max_epochs': 0, 'power': 0.9,
+        'optimizer': {
+            'type': 'SGD', 'lr': 0.01, 'momentum': 0.9, 'weight_decay': 5e-4, 'params_rules': {},
+        }
+    },
+    dataset=None,
+    dataloader=None,
+)
