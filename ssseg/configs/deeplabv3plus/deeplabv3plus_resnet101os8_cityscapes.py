73b2ad6b8f2d6d993b7bc86001f2c157a17ad96f
CharlesPikachu <1159254961@qq.com>
2025-06-04 20:53:39 +0800

    update configs

--- ssseg/configs/deeplabv3plus/deeplabv3plus_resnet101os8_cityscapes.py
+++ ssseg/configs/deeplabv3plus/deeplabv3plus_resnet101os8_cityscapes.py
@@ -1,16 +1,14 @@
 '''deeplabv3plus_resnet101os8_cityscapes'''
 import os
-import copy
-from .base_cfg import SEGMENTOR_CFG
-from .._base_ import DATASET_CFG_CITYSCAPES_512x1024, DATALOADER_CFG_BS8
+from .._base_ import REGISTERED_SEGMENTOR_CONFIGS, REGISTERED_DATASET_CONFIGS, REGISTERED_DATALOADER_CONFIGS
 
 
 # deepcopy
-SEGMENTOR_CFG = copy.deepcopy(SEGMENTOR_CFG)
+SEGMENTOR_CFG = REGISTERED_SEGMENTOR_CONFIGS['DEEPLABV3PLUS_SEGMENTOR_CFG'].copy()
 # modify dataset config
-SEGMENTOR_CFG['dataset'] = DATASET_CFG_CITYSCAPES_512x1024.copy()
+SEGMENTOR_CFG['dataset'] = REGISTERED_DATASET_CONFIGS['DATASET_CFG_CITYSCAPES_512x1024'].copy()
 # modify dataloader config
-SEGMENTOR_CFG['dataloader'] = DATALOADER_CFG_BS8.copy()
+SEGMENTOR_CFG['dataloader'] = REGISTERED_DATALOADER_CONFIGS['DATALOADER_CFG_BS8'].copy()
 # modify scheduler config
 SEGMENTOR_CFG['scheduler']['max_epochs'] = 220
 # modify other segmentor configs
